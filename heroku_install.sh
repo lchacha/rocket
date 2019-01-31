c5b771a78903b4b5b4db0cfc2abc910e31825e46
lchacha <lenah90ke@gmail.com>
2019-02-01 01:55:39 +0200

    Update heroku_install.sh

--- heroku_install.sh
+++ heroku_install.sh
@@ -41,3 +41,58 @@ SCRIPT
   echo "heroku cli installed to $LOCATION"
   heroku version
 }
+
+
+ # if apt-transport-https is not installed, clear out old sources, update, then install apt-transport-https
+
+  dpkg -s apt-transport-https 1>/dev/null 2>/dev/null || \
+
+    (echo "" > /etc/apt/sources.list.d/heroku.list \
+
+      && apt-get update \
+
+      && apt-get install -y apt-transport-https)
+
+
+
+  # add heroku repository to apt
+
+  echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
+
+
+
+  # remove toolbelt
+
+  (dpkg -s heroku-toolbelt 1>/dev/null 2>/dev/null && (apt-get remove -y heroku-toolbelt heroku || true)) || true
+
+
+
+  # install heroku's release key for package verification
+
+  wget -qO- https://cli-assets.heroku.com/apt/release.key | apt-key add -
+
+
+
+  # update your sources
+
+  apt-get update
+
+
+
+  # install the toolbelt
+
+  apt-get install -y heroku
+
+
+
+SCRIPT
+
+  # test the CLI
+
+  LOCATION=$(which heroku)
+
+  echo "heroku cli installed to $LOCATION"
+
+  heroku version
+
+}
